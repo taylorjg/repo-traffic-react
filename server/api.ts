759d8c8f7efcade27d4758ea9974182108adee68
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-11-20 15:55:43 +0000

    Return first page of repos

--- server/api.ts
+++ server/api.ts
@@ -1,9 +1,40 @@
+import axios from 'axios'
 import express from 'express'
 
-export const configureApi = (token: string) => {
+const parseLinkHeader = (response: express.Response) => {
+  const linkHeader = response.getHeaders()['link']
+  if (typeof linkHeader !== 'string' || !linkHeader) {
+    return []
+  }
+  return linkHeader
+    .split(',')
+    .map(link => link.split(';').map(s => s.trim()))
+    .map(([hrefPart, relPart]) => {
+      const hrefMatch = /^<([^>]+)>$/.exec(hrefPart ?? '')
+      const relMatch = /^rel="([^"]+)"$/.exec(relPart ?? '')
+      if (!hrefMatch || !relMatch) return null
+      const href = hrefMatch[1]
+      const rel = relMatch[1]
+      return { href, rel }
+    })
+    .filter(Boolean)
+}
+
+export const configureApi = (username: string, token: string) => {
+
+  axios.defaults.baseURL = 'https://api.github.com'
+  axios.defaults.headers.common['Accept'] = 'application/vnd.github.v3+json'
+  axios.defaults.headers.common['Authorization'] = `token ${token}`
 
   const getRepos = async (_req: express.Request, res: express.Response) => {
-    res.send(token)
+    const url = `/users/${username}/repos`
+    const config = {
+      params: {
+        per_page: 100
+      }
+    }
+    const { data } = await axios.get(url, config)
+    res.send(data)
   }
 
   const apiRouter = express.Router()
--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,15 @@ import 'dotenv/config'
 import express from 'express'
 import { configureApi } from './api'
 
-const apiRouter = configureApi(process.env.GITHUB_TOKEN ?? '')
+const username = process.env.GITHUB_USERNAME
+const token = process.env.GITHUB_TOKEN
+
+if (!username || !token) {
+  process.stderr.write('GITHUB_USERNAME and GITHUB_TOKEN both need to be defined!\n')
+  process.exit(1)
+}
+
+const apiRouter = configureApi(username, token)
 
 const app = express()
 app.use('/api', apiRouter)
