ce27cafa56c75ce29e39e16956c33ac7c6c3b20a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-11-20 23:08:25 +0000

    Fetch traffic in chunks

--- server/api.ts
+++ server/api.ts
@@ -18,7 +18,21 @@ const parseLinkHeader = (response: AxiosResponse) => {
     })
 }
 
-async function* getPagesGen(url: string, config: AxiosRequestConfig): AsyncGenerator {
+async function* splitEvery<T>(xs: AsyncGenerator<T>, n: number): AsyncGenerator<T[]> {
+  let chunk: T[] = []
+  for await (const x of xs) {
+    chunk.push(x)
+    if (chunk.length === n) {
+      yield chunk
+      chunk = []
+    }
+  }
+  if (chunk.length > 0) {
+    yield chunk
+  }
+}
+
+async function* getPagesGen(url: string, config: AxiosRequestConfig): AsyncGenerator<any> {
   console.log('[getPagesGen]', 'url:', url)
   const response = await axios.get(url, config)
   const repos = response.data
@@ -43,11 +57,32 @@ export const configureApi = (username: string, token: string) => {
         per_page: 100
       }
     }
-    const repos = []
-    for await (const repo of getPagesGen(url, config)) {
-      repos.push(repo)
+    const asyncIter = getPagesGen(url, config)
+    const CHUNK_SIZE = 50
+    const results: any[] = []
+    for await (const reposChunk of splitEvery(asyncIter, CHUNK_SIZE)) {
+      console.log('[getRepos]', 'reposChunk.length:', reposChunk.length)
+      const viewsPromises = reposChunk.map(repo => axios.get(`/repos/${repo.owner.login}/${repo.name}/traffic/views`))
+      const clonesPromises = reposChunk.map(repo => axios.get(`/repos/${repo.owner.login}/${repo.name}/traffic/clones`))
+      const fred = await Promise.all([...viewsPromises, ...clonesPromises])
+      reposChunk.forEach((repo: any, index: number) => {
+        results.push({
+          repo: {
+            name: repo.name,
+            description: repo.description,
+            stargazers_count: repo.stargazers_count,
+            forks_count: repo.forks_count,
+            created_at: repo.created_at,
+            updated_at: repo.updated_at,
+            html_url: repo.html_url,
+            language: repo.language
+          },
+          views: fred[index].data,
+          clones: fred[index + reposChunk.length].data
+        })
+      })
     }
-    res.send(repos)
+    res.send(results)
   }
 
   const apiRouter = express.Router()
