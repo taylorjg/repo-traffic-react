b4098447e36e2afa15a280654f001d0f5c43d74c
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-11-21 17:21:22 +0000

    Prepare for deployment to Heroku

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,23 @@
 import 'dotenv/config'
 import express from 'express'
+import path from 'path'
 import { configureApi } from './api'
+
+const BUILD_FOLDER = path.resolve(__dirname, '..', 'build')
+console.log('BUILD_FOLDER:', BUILD_FOLDER)
 
 const username = process.env.GITHUB_USERNAME
 const token = process.env.GITHUB_TOKEN
 
 if (!username || !token) {
-  process.stderr.write('GITHUB_USERNAME and GITHUB_TOKEN both need to be defined!\n')
+  process.stderr.write('GITHUB_USERNAME and GITHUB_TOKEN must be defined!\n')
   process.exit(1)
 }
 
 const apiRouter = configureApi(username, token)
 
 const app = express()
+app.use(express.static(BUILD_FOLDER))
 app.use('/api', apiRouter)
 
 const port = process.env.PORT || 5000
