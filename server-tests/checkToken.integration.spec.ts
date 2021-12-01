5d2244e7662792eccfce8a01003885108707177e
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-01 00:45:57 +0000

    Integration tests for checkToken.ts

---
+++ server-tests/checkToken.integration.spec.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import { checkToken } from '../server/checkToken'
+
+const {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  GITHUB_TOKEN_VALID,
+  GITHUB_TOKEN_REVOKED
+} = process.env
+
+describe('checkToken integration tests', () => {
+
+  it('when token is valid', async () => {
+    const checkTokenData = await checkToken(GITHUB_CLIENT_ID!, GITHUB_CLIENT_SECRET!, GITHUB_TOKEN_VALID!)
+    expect(checkTokenData).toBeDefined()
+  })
+
+  it('when token is revoked', async () => {
+    const checkTokenData = await checkToken(GITHUB_CLIENT_ID!, GITHUB_CLIENT_SECRET!, GITHUB_TOKEN_REVOKED!)
+    expect(checkTokenData).toBeUndefined()
+  })
+
+  it('when token is bogus', async () => {
+    const checkTokenData = await checkToken(GITHUB_CLIENT_ID!, GITHUB_CLIENT_SECRET!, 'bogus')
+    expect(checkTokenData).toBeUndefined()
+  })
+})
