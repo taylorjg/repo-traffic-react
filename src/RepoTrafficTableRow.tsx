6f88ab435a7d250476297bff301c60fc44b63f69
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-01-29 00:33:47 +0000

    Move repo url link to icon

--- src/RepoTrafficTableRow.tsx
+++ src/RepoTrafficTableRow.tsx
@@ -1,14 +1,30 @@
 import { TableCell, TableRow } from '@mui/material'
+import OpenInNew from '@mui/icons-material/OpenInNew'
 import { RepoData } from './types'
+import styled from '@emotion/styled'
 
 export type RepoTrafficTableRowProps = {
   repoData: RepoData
 }
 
+const AlignedCenter = styled.div`
+  display: flex;
+  align-items: center;
+`
+
 const RepoTrafficTableRow: React.FC<RepoTrafficTableRowProps> = ({ repoData }) => {
   return (
     <TableRow>
-      <TableCell><a href={repoData.htmlUrl}>{repoData.name}</a></TableCell>
+      <TableCell>
+        <AlignedCenter>
+          {repoData.name}
+          <a href={repoData.htmlUrl}>
+            <AlignedCenter>
+              <OpenInNew style={{ marginLeft: '.25rem', fontSize: 'small' }} />
+            </AlignedCenter>
+          </a>
+        </AlignedCenter>
+      </TableCell>
       <TableCell>{repoData.language}</TableCell>
       <TableCell>{repoData.viewsCount}</TableCell>
       <TableCell>{repoData.viewsUniques}</TableCell>
