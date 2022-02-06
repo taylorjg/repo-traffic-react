f322ed9b529e4a007b04ec16fef16e2b4c2cc71b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-02-06 01:14:29 +0000

    Fix unit tests

--- server-tests/apiImpl.integration.spec.ts
+++ server-tests/apiImpl.integration.spec.ts
@@ -1,74 +1,13 @@
 import 'dotenv/config'
-import axios from 'axios'
-import { getItemsGen, getReposImpl } from '../server/apiImpl'
+import { getReposImpl } from '../server/apiImpl'
 
 const {
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
-  GITHUB_TOKEN_VALID,
-  GITHUB_TOKEN_REVOKED
+  GITHUB_TOKEN_VALID
 } = process.env
 
 describe('apiImpl integration tests', () => {
-
-  describe('getItemsGen', () => {
-
-    const url = 'https://api.github.com/users/taylorjg/repos'
-
-    describe('when token is valid', () => {
-
-      const axiosInstance = axios.create({
-        baseURL: 'https://api.github.com',
-        headers: {
-          'Accept': 'application/vnd.github.v3+json',
-          'Authorization': `token ${GITHUB_TOKEN_VALID}`
-        }
-      })
-
-      it('when limit is specified', async () => {
-        const asyncItemsIter = getItemsGen(axiosInstance, url, {
-          pageSize: 100,
-          maxItems: 112
-        })
-        const items = []
-        for await (const item of asyncItemsIter) {
-          items.push(item)
-        }
-        expect(items).toHaveLength(112)
-      })
-
-      it('when no limit is specified', async () => {
-        const asyncItemsIter = getItemsGen(axiosInstance, url, {
-          pageSize: 100
-        })
-        const items = []
-        for await (const item of asyncItemsIter) {
-          items.push(item)
-        }
-        expect(items.length).toBeGreaterThan(200)
-      }, 20000)
-    })
-
-    describe('when token is revoked', () => {
-
-      const axiosInstance = axios.create({
-        baseURL: 'https://api.github.com',
-        headers: {
-          'Accept': 'application/vnd.github.v3+json',
-          'Authorization': `token ${GITHUB_TOKEN_REVOKED}`
-        }
-      })
-
-      it('throws when trying to access the first item', async () => {
-        const asyncItemsIter = getItemsGen(axiosInstance, url, {
-          pageSize: 100,
-          maxItems: 10
-        })
-        await expect(asyncItemsIter.next()).rejects.toThrow('Request failed with status code 401')
-      })
-    })
-  })
-
   describe('getReposImpl', () => {
     it('when limit is specified', async () => {
       const outcome = await getReposImpl(
--- server-tests/apiImpl.spec.ts
+++ server-tests/apiImpl.spec.ts
@@ -1,4 +1,4 @@
-import { asyncSplitEvery, parseLinkHeader } from '../server/apiImpl'
+import { asyncSplitEvery } from '../server/apiImpl'
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -23,19 +23,5 @@ describe('apiImpl unit tests', () => {
     expect(chunks[0]).toEqual([0, 1, 2, 3])
     expect(chunks[1]).toEqual([4, 5, 6, 7])
     expect(chunks[2]).toEqual([8, 9])
-  })
-
-  it('parseLinkHeader', () => {
-    const linkHeader = '<https://api.github.com/user/676165/repos?per_page=100&page=2>; rel="next", <https://api.github.com/user/676165/repos?per_page=100&page=3>; rel="last"'
-    const links = parseLinkHeader(linkHeader)
-    expect(links).toHaveLength(2)
-    expect(links[0]).toEqual({
-      href: 'https://api.github.com/user/676165/repos?per_page=100&page=2',
-      rel: 'next'
-    })
-    expect(links[1]).toEqual({
-      href: 'https://api.github.com/user/676165/repos?per_page=100&page=3',
-      rel: 'last'
-    })
   })
 })
